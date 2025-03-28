cef1fbeaaa8d7b56fb5d2fbcb981287156418402
aitor30 <aitor.rodriguez@stnicolau.com>
2025-03-28 15:42:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.body.style.opacity = "1";
-});
+    
+    let input = document.getElementById("campoTexto");
 
-document.getElementById("campoTexto").addEventListener("click", function() {
-    this.style.display = "none";
+    input.addEventListener("keydown", function() {
+        document.body.style.cursor = "none";
+    });
+
+    input.addEventListener("keyup", function() {
+        setTimeout(() => {
+            document.body.style.cursor = "default";
+        }, 1000);
+    });
 });
