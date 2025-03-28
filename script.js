42c347d1d81f912449fa28a92e287679e3abf647
aitor30 <aitor.rodriguez@stnicolau.com>
2025-03-28 15:40:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = "1";
+});
+
+document.getElementById("campoTexto").addEventListener("click", function() {
+    this.style.display = "none";
+});
